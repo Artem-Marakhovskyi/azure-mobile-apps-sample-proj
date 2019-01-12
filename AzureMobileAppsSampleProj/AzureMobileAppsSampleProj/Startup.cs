16fb6e2ced4dab9ae61a19b1b71aaed961dee407
Artem Marakhovskyi <Artem.marakhovskyi@gmail.com>
2019-01-12 11:55:07 +0200

    initial commit

---
+++ AzureMobileAppsSampleProj/AzureMobileAppsSampleProj/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Web.Http;
+using Microsoft.Azure.Mobile.Server.Config;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AzureMobileAppsSampleProj.Startup))]
+namespace AzureMobileAppsSampleProj
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+
+        public static void ConfigureMobileApp(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            var mobileConfig = new MobileAppConfiguration();
+
+            mobileConfig
+                .AddTablesWithEntityFramework()
+                .ApplyTo(config);
+
+            Database.SetInitializer(new MobileServiceInitializer());
+
+            app.UseWebApi(config);
+        }
+    }
+
+    public class MobileServiceInitializer : CreateDatabaseIfNotExists<MobileServiceContext>
+    {
+        protected override void Seed(MobileServiceContext context)
+        {
+            List<TodoItem> todoItems = new List<TodoItem>
+            {
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "First item", Complete = false },
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "Second item", Complete = false }
+            };
+
+            foreach (TodoItem todoItem in todoItems)
+            {
+                context.Set<TodoItem>().Add(todoItem);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
